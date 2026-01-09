4f3ea7ac9d8a044b5ae3062d12f4f466be12773e
danielaranadj-prog <danielaranadj@gmail.com>
2026-01-08 23:16:42 -0700

    hola

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://www.instantetrips.com',
+  base: '/destinos_argentina', // <--- Cambia esto al nombre actual
+});
