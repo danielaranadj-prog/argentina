ff2ac1f39301b0162d6eda3a3edcf70a1764d0f8
danielaranadj-prog <danielaranadj@gmail.com>
2026-01-10 21:57:21 -0700

    hola

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	// Esto le dice a Tailwind qué archivos mirar para generar estilos
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				// Aquí registramos tu fuente. 
-				// Cuando escribas 'font-montserrat', Tailwind usará esto:
-				montserrat: ['Montserrat', 'sans-serif'],
-			},
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'], // Esto forzará la fuente en todo
+      },
+			colors: {
+        // Definimos tus colores aquí para que Tailwind los entienda
+        'arg-dark': '#0b1116',
+        'arg-card': '#161f29',
+        'arg-celeste': '#75AADB',
+        'arg-sol': '#F4B942',
+      }
 		},
 	},
 	plugins: [
-		// Este plugin es vital para que los artículos del blog (.md) se vean bonitos
 		require('@tailwindcss/typography'),
 	],
 }
