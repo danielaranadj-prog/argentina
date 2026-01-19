aae32d403c7635fa0849f439840e84d481937758
danielaranadj-prog <danielaranadj@gmail.com>
2026-01-18 19:07:31 -0700

    Rename all paths from destinos_argentina to argentina

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://www.instantetrips.com',
-  base: '/destinos_argentina', // <--- Cambia esto al nombre actual
+  base: '/argentina',
 });
